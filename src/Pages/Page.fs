423875ba2ffd39b7a94f2d65c6ceb6a759c748ae
Freddie Sackur <git@sckr.link>
2026-06-01 14:43:02 +0100

    wip on create-jot page

--- src/Pages/Page.fs
+++ src/Pages/Page.fs
@@ -2,24 +2,39 @@ module Jot.Pages.Page
 
 open Feliz
 open ElmishLand
+open Jot.Domain
 open Jot.Shared
 open Jot.Pages
 
-type Model = unit
+type Model = Jot
 
 type Msg =
     | LayoutMsg of Layout.Msg
+    | Create of Jot
 
 let init () =
-    (),
+    Unchecked.defaultof<_>,
     Command.none
 
 let update (msg: Msg) (model: Model) =
     match msg with
     | LayoutMsg _ -> model, Command.none
+    | Create jot -> jot, Command.none
 
 let view (_model: Model) (_dispatch: Msg -> unit) =
-    Html.text " Page"
+    Html.form [
+        prop.id "createJot"
+        prop.children [
+            Html.label [
+                prop.children [
+                    Html.text "Summary"
+                    Html.input [
+                        prop.name "Summary"
+                    ]
+                ]
+            ]
+        ]
+    ]
 
 let page (_shared: SharedModel) (_route: HomeRoute) =
     Page.from init update view () LayoutMsg
